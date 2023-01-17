fc0b795817bbf7b83a82c4af6b0292a56532cac7
saaut <saaut60@naver.com>
2023-01-17 21:55:45 +0900

    Interface_Compelete2

--- app/src/main/java/com/example/signalussample1/fragment/voiceToTextFragment.kt
+++ app/src/main/java/com/example/signalussample1/fragment/voiceToTextFragment.kt
@@ -9,6 +9,8 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.signalussample1.R
 import kotlinx.android.synthetic.main.fragment_second_select_face.*
+import kotlinx.android.synthetic.main.fragment_second_select_face.back_btn
+import kotlinx.android.synthetic.main.fragment_voice_to_text.*
 
 /**
  * A simple [Fragment] subclass.
@@ -29,11 +31,15 @@ class voiceToTextFragment : Fragment(), View.OnClickListener {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
         back_btn.setOnClickListener(this)
+        again_txt.setOnClickListener(this)
 
     }
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.back_btn -> {
+                navController.popBackStack()
+            }
+            R.id.again_txt->{
                 navController.popBackStack()
             }
         }
